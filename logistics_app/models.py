8fff6610750f6f973ec54fdec19cc491427a2f5d
Ronnie95 <t_daron@yahoo.com>
2026-06-21 23:35:28 -0400

    user model profile creation

--- logistics_app/models.py
+++ logistics_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+
+    ROLE_CHOICES = (
+        ("SUPERVISOR", "Supervisor"),
+        ("DRIVER", "Driver "),
+        ("ROUTER", "ROUTER"),
+        ("MAINTENANCE", "MAINTENANCE"),
+    )
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    def __str__(self):
+        return self.user.username
