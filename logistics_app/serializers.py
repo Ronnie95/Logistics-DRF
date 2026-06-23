95636eb858239cfdd136bd90c8a1f8bbf6365625
Ronnie95 <t_daron@yahoo.com>
2026-06-23 12:52:55 -0400

    serializer updates

--- logistics_app/serializers.py
+++ logistics_app/serializers.py
@@ -1,17 +1,20 @@
-from .models import Routes, Delivery
+from .models import Routes, Delivery, Trailer, Truck
 from rest_framework import serializers, 
 from rest_framework.serializers import ModelSerializer
 
+class TruckSerializer(ModelSerializer):
+    model = Truck 
+    fields = ('truck_number', 'make', 'model', 'year', 'truck_options')
 
-
-class DeliverySerializer(ModelSerializer):
+class TrailerSerializer(ModelSerializer):
+    model = Trailer 
+    fields = ('trailer_number', 'trailer_option')
     class Meta:
         model = Delivery
         fields = ('id','stop','customer_name', 'delivery_window', 'pickup_location', 'dropoff_location', 'order_number', 'status') 
 
 
-
 class RoutesSerializer(ModelSerializer):
     class Meta:
         model = Routes
-        fields = ('id','route_name','deliveries') 
+        fields = ('id','route_name','route_date', 'trucks', 'trailers') 
