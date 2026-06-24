875f0423684aaeeacf7adaf7feabbe6d3e8f50a1
Ronnie95 <t_daron@yahoo.com>
2026-06-24 00:59:35 -0400

    view updates

--- logistics_app/views.py
+++ logistics_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Trailer, Truck, Routes, CustomerInfo, DeliverExceptions, Delivery, HOSLog, PreTripInspection, InspectionItem, MaintenanceItem, MaintenanceRecord
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MaintenanceRecordSerializer, MaintenanceItemSerializer, InspectionItemSerializer, PreTripSerializer, HOSLogSerializer, DeliverExceptionSerializer, CustomerInfoSerializer, TrailerSerializer, TruckSerializer, DeliverySerializer, RoutesSerializer
 # Create your views here.
+
