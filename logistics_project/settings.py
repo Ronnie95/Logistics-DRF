b401c397da7455d87ed03d74247b9731f99d6a67
Ronnie95 <t_daron@yahoo.com>
2026-07-05 23:41:34 -0400

    bug fixes

--- logistics_project/settings.py
+++ logistics_project/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'logistics_app',
-    'rest_framework', 
+    'rest_framework',
+    'corsheaders',
+    
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
